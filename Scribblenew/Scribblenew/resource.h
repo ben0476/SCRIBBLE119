2efc888aedc8ea3f87a7977225e6975a827cac60
ben0476 <1453ben@gmail.com>
2018-11-15 15:24:14 +0800

    Canvas Done

---
+++ Scribblenew/Scribblenew/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Scribblenew.rc
+//
+#define CANCEL                          2
+#define ID_CANCEL                       2
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ScribblenewTYPE             130
+#define ID_WINDOW_MANAGER               131
+#define IDD_PEN_WIDTHS                  311
+#define IDD_CANVAS                      312
+#define IDCANCEL                        1000
+#define IDC_DEFAULT                     1001
+#define IDC_THIN_PEN_EIDTH              1002
+#define IDC_THICK_PEN_WIDTH             1003
+#define IDC_SLIDER1                     1004
+#define IDC_THIN_SLIDER                 1004
+#define IDC_SLIDER2                     1005
+#define IDC_THICK_SLIDER                1005
+#define IDC_Background_Color            1007
+#define IDC_Canvas_Width                1008
+#define IDC_Canvas_Height               1009
+#define IDC_Color_Pick                  1010
+#define ID_PEN_THICKLINE                32771
+#define ID_PEN_WIDTHS                   32772
+#define ID_PEN_CLEARALL                 32773
+#define ID_PEN_CLEARALL32774            32774
+#define ID_PEN_Color                    32777
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
